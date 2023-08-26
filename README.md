5351f93e169aaa213f6f7d353983f1bf4923383a
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-08-27 00:13:42 +0200

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-In progress
+# In progress
+
+Current implementation uses round robin logic for distributing requests.
+
+For testing,
+
+run the load balancer:
+- `./lb`
+
+run the servers:
+- `python3 echoer.py 8081`
+- `python3 echoer.py 8082`
+- `python3 echoer.py 8083`
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,19 +7,16 @@ import pureconfig.generic.auto._
 import scala.util.Try
 
 object Main extends IOApp {
+
+  private final case class Config(port: String, backends: Backends)
+
   override def run(args: List[String]): IO[ExitCode] =
     (for {
       config   <- IO.delay(ConfigSource.default.loadOrThrow[Config])
       backends <- Ref.of[IO, Backends](config.backends)
-      port     <- IO.fromOption(maybePort(config.port))(new RuntimeException("invalid port"))
+      portInt  =  Try(config.port.toInt).toOption.getOrElse(8080)
+      port     <- IO.fromOption(Port.fromInt(portInt))(new RuntimeException("invalid port"))
       _        <- LoadbalancerServer.run(backends, port)
     } yield ()).as(ExitCode.Success)
 
-  private def maybePort(portStr: String): Option[Port] =
-    for {
-      portInt   <- Try(portStr.toInt).toOption
-      maybePort <- Port.fromInt(portInt)
-    } yield maybePort
-
-  private final case class Config(port: String, backends: Backends)
 }
