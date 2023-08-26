d2ad2842b916f39651365603171d2e56cf006a02
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-08-27 00:05:05 +0200

    add thread safe mutable reference

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,22 +9,17 @@ import scala.util.Try
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
     (for {
-      cfg <- IO.fromOption(ConfigSource.default.load[Config].toOption) {
-        new RuntimeException("Could not load config")
-      }
-      backends <- Ref.of[IO, Backends](cfg.backends)
-      port <- IO.fromOption(maybePort(args.headOption.getOrElse("8080"))) {
-        new RuntimeException("Could not construct port")
-      }
-      _ <- LoadbalancerServer.run(backends, port)
+      config   <- IO.delay(ConfigSource.default.loadOrThrow[Config])
+      backends <- Ref.of[IO, Backends](config.backends)
+      port     <- IO.fromOption(maybePort(config.port))(new RuntimeException("invalid port"))
+      _        <- LoadbalancerServer.run(backends, port)
     } yield ()).as(ExitCode.Success)
 
-  private def maybePort(portStr: String): Option[Port] = {
+  private def maybePort(portStr: String): Option[Port] =
     for {
-      portInt <- Try(portStr.toInt).toOption
-      port <- Port fromInt portInt
-    } yield port
-  }
+      portInt   <- Try(portStr.toInt).toOption
+      maybePort <- Port.fromInt(portInt)
+    } yield maybePort
 
-  final case class Config(port: String, backends: Backends)
+  private final case class Config(port: String, backends: Backends)
 }
