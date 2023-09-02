0f84e65e1477d799d21466d93c6649da6ad5115a
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-09-02 13:35:16 +0200

    add RoundRobin tests

--- src/test/scala/com/ghurtchu/loadbalancer/RoundRobinTest.scala
+++ src/test/scala/com/ghurtchu/loadbalancer/RoundRobinTest.scala
@@ -14,11 +14,9 @@ class RoundRobinTest extends FunSuite {
     (for {
       ref <- IO.ref(urls)
       backends = Backends(ref)
-      result1 <- roundRobin(backends)
-      result2 <- roundRobin(backends)
-    } yield assert {
-      result1.exists(_.value == "localhost:8082") && result2.exists(_.value == "localhost:8082")
-    })
+      assertion1 <- roundRobin(backends).map(_.exists(_.value == "localhost:8082"))
+      assertion2 <- roundRobin(backends).map(_.exists(_.value == "localhost:8082"))
+    } yield assert(List(assertion1, assertion2).forall(_ == true)))
       .unsafeRunSync()
   }
 
@@ -65,12 +63,17 @@ class RoundRobinTest extends FunSuite {
     val urls       = Urls(Vector("localhost:8081", "localhost:8082"))
 
     (for {
-      ref     <- IO.ref(urls)
-      result1 <- roundRobin(HealthChecks(ref))
-      result2 <- roundRobin(HealthChecks(ref))
-    } yield assert {
-      result1.value == "localhost:8081" && result2.value == "localhost:8082"
-    })
+      ref <- IO.ref(urls)
+      healthChecks = HealthChecks(ref)
+      assertion1 <- roundRobin(healthChecks)
+        .map(_.value == "localhost:8081")
+      assertion2 <- ref.get
+        .map(_.values.map(_.value) == Vector("localhost:8082", "localhost:8081"))
+      assertion3 <- roundRobin(healthChecks)
+        .map(_.value == "localhost:8082")
+      assertion4 <- ref.get
+        .map(_.values.map(_.value) == Vector("localhost:8081", "localhost:8082"))
+    } yield assert(List(assertion1, assertion2, assertion3, assertion4).forall(_ == true)))
       .unsafeRunSync()
   }
 
